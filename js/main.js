39ee2169b19d7681e3da1284ed5ba8fd58e7e210
Aidajy111 <aidasjy4.00@gmail.com>
2024-12-15 16:00:53 +0800

    Route

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ const openProfileModalBody = $('#openProfileModalBody')
 openProfileModal.on('click', function () {
 	openProfileModalBody.toggleClass('active')
 })
+
+$('#eventSlid-1').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+})
+$('#eventSlid-2').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+})
+$('.rote-card__slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: true,
+})
+
+var dntSaveRoute = $('.dnt-save-route')
+
+$('.dnt-save-route').click(function () {
+	alert('Маршрут добавлен в ваши маршруты')
+})
+$('#createRoute').on('click', function () {
+	window.location.replace('/my-route.html')
+	return false
+})
